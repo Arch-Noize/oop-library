6f7ceed50bbfb56f17dc0ab48caa482a524a79ff
Arch-Noize <santiagonoize@gmail.com>
2023-11-07 08:28:17 -0400

    Add reviewer changes

--- student.rb
+++ student.rb
@@ -3,10 +3,14 @@ require_relative 'person'
 class Student < Person
   def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
     @classroom = classroom
-    super(id, age: age, name: name, parent_permission: parent_permission)
+    super(age: age, name: name, parent_permission: parent_permission)
   end
 
   def play_hooky
     '¯(ツ)/¯'
   end
 end
+
+student = Student.new(age: 26, name: 'Paula', parent_permission: false, classroom: 'Grammar')
+
+puts student.name
